8b5644bb1711ffe0ee46c1162ff0b90907ac7883
MHD KHAIR SULTAN <sultanmohammad94@gmail.com>
2022-10-18 10:02:51 +0300

    add unit test to python_Add.py

--- python_Add.py
+++ python_Add.py
@@ -1,5 +1,22 @@
-#Print the sum of two numbers
-n1 = int(input("Enter the first number"))
-n2 = int(input("Enter the Second number"))
+from unittest import TestCase
+import unittest
 
-print("Sum = {}".format(n1+n2))
+
+class TestCalculator(TestCase):
+   def test_add(self): 
+    '''Test if the add operation work correclty.'''
+    n1 = 5
+    n2 = 10
+    self.assertEqual(15, n1 + n2)
+    self.assertEqual(1, False + 1)
+    self.assertEqual(2,  True + 1)
+    self.assertEqual(1, True + 0)
+    self.assertEqual(0, False + 0)
+    self.assertEqual(1, True + False)
+        
+    with self.assertRaises(TypeError):
+        "4" + 1
+        
+
+if __name__ == "__main__":
+    unittest.main()
